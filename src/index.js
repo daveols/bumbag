156faf331f1f9cee63cd374519b788a8f5efb539
Dave Olsen <daivols@gmail.com>
2018-10-18 17:15:15 +1100

    feat(wip): rating component

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ export { Link } from './Link';
 export { List, ListItem } from './List';
 export { Navigation } from './Navigation';
 export { Paragraph } from './Paragraph';
+export { Rating } from './Rating';
 export { Spinner } from './Spinner';
 export { Table, TableBody, TableCaption, TableCell, TableFoot, TableHead, TableHeadCell, TableRow } from './Table';
 export { Text } from './Text';
